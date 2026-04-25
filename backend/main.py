0f371c5d601c5398aea9b6fc881d6e864b659bb7
simonwsesuraj <simonwsesuraj@gmail.com>
2026-04-25 19:17:01 +0530

    first-commit

---
+++ backend/main.py
@@ -0,0 +1,69 @@
+from pyexpat import features
+
+import pandas as pd
+import numpy as np
+from fastapi import FastAPI
+from pydantic import BaseModel,Field
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+model_path = os.path.join("model","Salary_model.pkl")
+model = joblib.load(model_path) 
+
+
+class SalaryInput(BaseModel):
+    age:int
+    gender:str
+    education_level:str
+    job_title:str
+    year_of_experience:int
+
+@app.get("/")
+def home():
+    return {"message": "Salary Prediction API Running"}
+
+@app.post("/predict")
+def predict_salary(data:SalaryInput):
+    try:
+        input_dict = {
+        "Age": data.age,
+        "Gender": data.gender,
+        "Education Level": data.education_level,
+        "Job Title": data.job_title,
+        "Years of Experience": data.year_of_experience
+        }
+
+        df = pd.DataFrame([input_dict])
+
+        prediction = model.predict(df)
+        return {"predicted_salary": float(prediction[0])}
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+
+
+data_path = os.path.join("data.csv")
+df = pd.read_csv(data_path)
+
+
+education_levels = sorted(df['Education Level'].dropna().unique().tolist())
+job_titles = sorted(df['Job Title'].dropna().unique().tolist())
+
+@app.get("/options")
+def get_options():
+    return {
+        "education_levels": education_levels,
+        "job_titles": job_titles
+    }
---
+++ frontend/src/App.jsx
@@ -0,0 +1,117 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import { FaBriefcase } from "react-icons/fa";
+
+function App() {
+  const [age, setAge] = useState('')
+  const [gender, setGender] = useState('')
+  const [educationLevel, setEducationLevel] = useState('')
+  const [jobTitle, setJobTitle] = useState('')
+  const [yearsOfExperience, setYearsOfExperience] = useState('')
+  const [salary, setSalary] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [educationOptions, setEducationOptions] = useState([])
+  const [jobOptions, setJobOptions] = useState([])
+
+  useEffect(() => {
+    const fetchOptions = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/options')
+        const data = await res.json()
+        setEducationOptions(data.education_levels)
+        setJobOptions(data.job_titles)
+      } catch (err) {
+        console.error("Error loading dropdown data", err)
+      }
+    }
+
+    fetchOptions()
+  }, [])
+
+  const predictSalary = async () => {
+    if (!age || !gender || !educationLevel || !jobTitle || !yearsOfExperience) {
+      alert("Please fill all fields")
+      return
+    }
+
+    setLoading(true)
+
+    try {
+      const response = await fetch('http://localhost:8000/predict', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          age: parseInt(age),
+          gender,
+          education_level: educationLevel,
+          job_title: jobTitle,
+          year_of_experience: parseInt(yearsOfExperience)
+        })
+      })
+
+      const data = await response.json()
+      setSalary(data.predicted_salary)
+
+    } catch (error) {
+      console.error("Error:", error)
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <div className="app">
+      <div className="card">
+        <h1 className="title">
+          <FaBriefcase className="icon" /> Employee Salary Prediction
+        </h1>
+
+        <input
+          type="number"
+          placeholder="Age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="">Select Gender</option>
+          <option>Male</option>
+          <option>Female</option>
+        </select>
+
+        <select value={educationLevel} onChange={(e) => setEducationLevel(e.target.value)}>
+          <option value="">Education Level</option>
+          {educationOptions.map((edu, i) => (
+            <option key={i}>{edu}</option>
+          ))}
+        </select>
+
+        <select value={jobTitle} onChange={(e) => setJobTitle(e.target.value)}>
+          <option value="">Job Title</option>
+          {jobOptions.map((job, i) => (
+            <option key={i}>{job}</option>
+          ))}
+        </select>
+
+        <input
+          type="number"
+          placeholder="Years of Experience"
+          value={yearsOfExperience}
+          onChange={(e) => setYearsOfExperience(e.target.value)}
+        />
+
+        <button onClick={predictSalary}>
+          {loading ? "Predicting..." : "Predict Salary"}
+        </button>
+
+        {salary && (
+          <div className="result">
+            ₹ {salary.toFixed(2)}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
