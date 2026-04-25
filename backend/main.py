438f334406689218fae9cf1c73e6b2a093b37fda
simonwsesuraj <simonwsesuraj@gmail.com>
2026-04-25 19:35:32 +0530

    url-updated

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["salary-prediction-react-fast-api.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
